eb9a98659babe7b69b9324de3c9ba4344e9bbb10
Hareigns <jarvin3355@gmail.com>
2025-03-10 18:38:17 +0100

    Listado de labs futuros (no tiene funcion de clic implementadas)

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,29 @@
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import AppLogo from "../assets/images/logoApp.webp"
+import "../assets/styles/header.css"
 
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className="mb-4">
       <Container>
-        <Navbar.Brand as={Link} to="/">Arquitecturas de Máquinas</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+
+          <img
+
+            src={AppLogo} // Usa la variable de la imagen importada
+
+            alt="Logo"
+
+            width="50px" // Ajusta el tamaño según sea necesario
+
+            height="50px" // Ajusta el tamaño según sea necesario
+
+            className="d-inline-block align-left"
+
+          />
+
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,143 @@
-import { Container } from "react-bootstrap";
-import { Card } from 'react-bootstrap';
+import React from "react";
+import "../assets/styles/home.css";
+import logoLab_1 from "../assets/images/logoLab_1.webp"
 
 const Home = () => {
   return (
-    <Container className="mt-5">
-      <Card>
-        <Card.Body>
-          <Card.Title>Bienvenido a Arquitecturas de Máquinas</Card.Title>
-          <Card.Text>
-            Explora las diferentes arquitecturas de computadoras y su evolución.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      
-    </Container>
+    <div class="container-home">
+      <div class="container-labs">
+
+        <div class="lab-container">
+          <div className="logo-lab">
+            <img src={logoLab_1} alt="Logo" width="250px" height="100px"></img>
+          </div>
+          <div className="info-lab">
+            <h6>Laboratorio: #1</h6>
+            <h6>Fecha: 08-03-25</h6>
+          </div>
+        </div>
+
+        <div class="lab-container">
+          <div className="logo-lab">
+            <img src={logoLab_1} alt="Logo" width="250px" height="100px"></img>
+          </div>
+          <div className="info-lab">
+            <h6>Laboratorio: #</h6>
+            <h6>Fecha: 00-00-25</h6>
+          </div>
+        </div>
+
+        <div class="lab-container">
+          <div className="logo-lab">
+            <img src={logoLab_1} alt="Logo" width="250px" height="100px"></img>
+          </div>
+          <div className="info-lab">
+            <h6>Laboratorio: #</h6>
+            <h6>Fecha: 00-00-25</h6>
+          </div>
+        </div>
+
+        <div class="lab-container">
+          <div className="logo-lab">
+            <img src={logoLab_1} alt="Logo" width="250px" height="100px"></img>
+          </div>
+          <div className="info-lab">
+            <h6>Laboratorio: #</h6>
+            <h6>Fecha: 00-00-25</h6>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="container-labs">
+
+        <div class="lab-container">
+          <div className="logo-lab">
+            <img src={logoLab_1} alt="Logo" width="250px" height="100px"></img>
+          </div>
+          <div className="info-lab">
+            <h6>Laboratorio: #</h6>
+            <h6>Fecha: 00-00-25</h6>
+          </div>
+        </div>
+
+        <div class="lab-container">
+          <div className="logo-lab">
+            <img src={logoLab_1} alt="Logo" width="250px" height="100px"></img>
+          </div>
+          <div className="info-lab">
+            <h6>Laboratorio: #</h6>
+            <h6>Fecha: 00-00-25</h6>
+          </div>
+        </div>
+
+        <div class="lab-container">
+          <div className="logo-lab">
+            <img src={logoLab_1} alt="Logo" width="250px" height="100px"></img>
+          </div>
+          <div className="info-lab">
+            <h6>Laboratorio: #</h6>
+            <h6>Fecha: 00-00-25</h6>
+          </div>
+        </div>
+
+        <div class="lab-container">
+          <div className="logo-lab">
+            <img src={logoLab_1} alt="Logo" width="250px" height="100px"></img>
+          </div>
+          <div className="info-lab">
+            <h6>Laboratorio: #</h6>
+            <h6>Fecha: 00-00-25</h6>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="container-labs">
+
+        <div class="lab-container">
+          <div className="logo-lab">
+            <img src={logoLab_1} alt="Logo" width="250px" height="100px"></img>
+          </div>
+          <div className="info-lab">
+            <h6>Laboratorio: #</h6>
+            <h6>Fecha: 00-00-25</h6>
+          </div>
+        </div>
+
+        <div class="lab-container">
+          <div className="logo-lab">
+            <img src={logoLab_1} alt="Logo" width="250px" height="100px"></img>
+          </div>
+          <div className="info-lab">
+            <h6>Laboratorio: #</h6>
+            <h6>Fecha: 00-00-25</h6>
+          </div>
+        </div>
+
+        <div class="lab-container">
+          <div className="logo-lab">
+            <img src={logoLab_1} alt="Logo" width="250px" height="100px"></img>
+          </div>
+          <div className="info-lab">
+            <h6>Laboratorio: #</h6>
+            <h6>Fecha: 00-00-25</h6>
+          </div>
+        </div>
+
+        <div class="lab-container">
+          <div className="logo-lab">
+            <img src={logoLab_1} alt="Logo" width="250px" height="100px"></img>
+          </div>
+          <div className="info-lab">
+            <h6>Laboratorio: #</h6>
+            <h6>Fecha: 00-00-25</h6>
+          </div>
+        </div>
+
+      </div>
+
+    </div >
   );
 };
 
