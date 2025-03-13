dd59636a070d82dec84b151f4153cb6449a4bbf5
Hareigns <jarvin3355@gmail.com>
2025-03-12 21:17:20 -0600

    View responsive de HOME (las demas no estan completas, ni responsive, tampoco hay funcion de clic a los laboratorios enlistados)

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,13 +2,13 @@ import { Container } from "react-bootstrap";
 import { Card } from 'react-bootstrap';
 
 const About = () => {
-    return (
-      <Container className="mt-5">
-        <Card>
+  return (
+    <Container className="mt-5">
+      <Card>
         <Card.Body>
           <Card.Title>Sobre Nosotros</Card.Title>
           <Card.Text>
-          Somos un equipo de dos apasionados estudiantes de la Universidad Nacional de Ingeniería (UNI), actualmente cursando la asignatura de Arquitectura de Máquinas I. Este proyecto nace como parte de nuestro trabajo de fin de curso, con el objetivo principal de compartir y difundir el conocimiento que hemos adquirido a lo largo de las clases teóricas y prácticas.
+            Somos un equipo de dos apasionados estudiantes de la Universidad Nacional de Ingeniería (UNI), actualmente cursando la asignatura de Arquitectura de Máquinas I. Este proyecto nace como parte de nuestro trabajo de fin de curso, con el objetivo principal de compartir y difundir el conocimiento que hemos adquirido a lo largo de las clases teóricas y prácticas.
           </Card.Text>
         </Card.Body>
       </Card>
@@ -16,18 +16,18 @@ const About = () => {
         <Card.Body>
           <Card.Title>¿Por qué lo hacemos?</Card.Title>
           <Card.Text>
-          La Arquitectura de Máquinas es una disciplina fundamental en el campo de la ingeniería de sistemas y computación. A través de este proyecto, buscamos:
+            La Arquitectura de Máquinas es una disciplina fundamental en el campo de la ingeniería de sistemas y computación. A través de este proyecto, buscamos:
 
-Organizar y sintetizar la información recopilada en clases y laboratorios.
+            Organizar y sintetizar la información recopilada en clases y laboratorios.
 
-Compartir conocimiento de manera didáctica y comprensible.
+            Compartir conocimiento de manera didáctica y comprensible.
 
-Fomentar el aprendizaje colaborativo, invitando a otros estudiantes a contribuir y enriquecer este espacio.
+            Fomentar el aprendizaje colaborativo, invitando a otros estudiantes a contribuir y enriquecer este espacio.
           </Card.Text>
         </Card.Body>
       </Card>
-      </Container>
-    );
-  };
-  
-  export default About;
+    </Container>
+  );
+};
+
+export default About;
--- src/pages/home.js
+++ src/pages/home.js
@@ -4,6 +4,17 @@ import logoLab_1 from "../assets/images/logoLab_1.webp"
 
 const Home = () => {
   return (
+    <div class="container-main">
+
+      <div class="container-info">
+        <h1>Arquitectura de Máquinas I</h1>
+        <p>En este sitio web podras encontrar información sobre el curso de Arquitectura de Máquinas I, que se llevará a cabo durante el primer semestre del 2023 en la Universidad Nacional de Ingeniería.
+          <br />
+          <br />
+          En los diferentes Laboratorios, podrás encontrar materiales y recursos útiles para tu aprendizaje, desde el punto de vista de la simplicidad para tener un mejor conocimiento de los conceptos y técnicas que se abordan en el curso.
+        </p>
+      </div>
+
     <div class="container-home">
       <div class="container-labs">
 
@@ -47,30 +58,6 @@ const Home = () => {
           </div>
         </div>
 
-      </div>
-
-      <div class="container-labs">
-
-        <div class="lab-container">
-          <div className="logo-lab">
-            <img src={logoLab_1} alt="Logo" width="250px" height="100px"></img>
-          </div>
-          <div className="info-lab">
-            <h6>Laboratorio: #</h6>
-            <h6>Fecha: 00-00-25</h6>
-          </div>
-        </div>
-
-        <div class="lab-container">
-          <div className="logo-lab">
-            <img src={logoLab_1} alt="Logo" width="250px" height="100px"></img>
-          </div>
-          <div className="info-lab">
-            <h6>Laboratorio: #</h6>
-            <h6>Fecha: 00-00-25</h6>
-          </div>
-        </div>
-
         <div class="lab-container">
           <div className="logo-lab">
             <img src={logoLab_1} alt="Logo" width="250px" height="100px"></img>
@@ -104,6 +91,60 @@ const Home = () => {
             <h6>Fecha: 00-00-25</h6>
           </div>
         </div>
+
+        <div class="lab-container">
+          <div className="logo-lab">
+            <img src={logoLab_1} alt="Logo" width="250px" height="100px"></img>
+          </div>
+          <div className="info-lab">
+            <h6>Laboratorio: #</h6>
+            <h6>Fecha: 00-00-25</h6>
+          </div>
+        </div>
+
+        <div class="lab-container">
+          <div className="logo-lab">
+            <img src={logoLab_1} alt="Logo" width="250px" height="100px"></img>
+          </div>
+          <div className="info-lab">
+            <h6>Laboratorio: #</h6>
+            <h6>Fecha: 00-00-25</h6>
+          </div>
+        </div>
+
+        <div class="lab-container">
+          <div className="logo-lab">
+            <img src={logoLab_1} alt="Logo" width="250px" height="100px"></img>
+          </div>
+          <div className="info-lab">
+            <h6>Laboratorio: #</h6>
+            <h6>Fecha: 00-00-25</h6>
+          </div>
+        </div>
+
+        <div class="lab-container">
+          <div className="logo-lab">
+            <img src={logoLab_1} alt="Logo" width="250px" height="100px"></img>
+          </div>
+          <div className="info-lab">
+            <h6>Laboratorio: #</h6>
+            <h6>Fecha: 00-00-25</h6>
+          </div>
+        </div>
+
+        <div class="lab-container">
+          <div className="logo-lab">
+            <img src={logoLab_1} alt="Logo" width="250px" height="100px"></img>
+          </div>
+          <div className="info-lab">
+            <h6>Laboratorio: #</h6>
+            <h6>Fecha: 00-00-25</h6>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="container-labs">
 
         <div class="lab-container">
           <div className="logo-lab">
@@ -138,6 +179,8 @@ const Home = () => {
       </div>
 
     </div >
+
+    </div>
   );
 };
 
