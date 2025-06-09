01f47fd7058b03f45ad27f09c980aedbead9ee1e
Hareigns <jarvin3355@gmail.com>
2025-06-09 09:04:17 -0600

    se agregaron los labs 9,10,11,12 (estan vacos, aun no los ha hecho JUDE)

---
+++ src/pages/lab9/Laboratorio9.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Laboratorio9 = () => {
+    return (
+        <div className="container">
+            <h1>Laboratorio 9</h1>
+            <p>Descripción detallada del Laboratorio 9.</p>
+            <p><strong>Fecha:</strong> 01/01/2025</p>
+        </div>
+    );
+};
+
+export default Laboratorio9;
