45f9e05884c6dd84cb5cd2fef1935180861778e2
Hareigns <jarvin3355@gmail.com>
2025-06-12 19:20:15 -0600

    Ya sirve y navbar en vistas pantallaschicas y ya solo falta modificar la info introductoria a la vista de cada lab correspondiente

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,71 @@
 import { Link } from 'react-router-dom';
 import AppLogo from "../assets/images/logoApp.webp";
 import "../assets/styles/navbar.css"; 
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Header = () => { 
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-
-<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className={`navbar navbar-expand-lg navbar-dark bg-dark ${scrolled ? 'scrolled' : ''}`}>
       <div className="container">
         <Link className="navbar-brand" to="/">
-          <img
-            src={AppLogo}
-            alt="Logo Veterinaria Jackson"
-          />
+          <img src={AppLogo} alt="Logo Arquitectura de Máquinas" />
           Arquitectura de Maquinas I
         </Link>
+        
         <button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav" 
-          aria-expanded="false"
+          onClick={toggleMenu}
+          aria-expanded={isMenuOpen}
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+
+        <div className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`} id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link" to="/">Inicio</Link>
+              <Link 
+                className="nav-link" 
+                to="/" 
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Inicio
+              </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/laboratorios">Laboratorios</Link>
+              <Link 
+                className="nav-link" 
+                to="/laboratorios" 
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Laboratorios
+              </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">Acerca de nosotros</Link>
+              <Link 
+                className="nav-link" 
+                to="/about" 
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Acerca de nosotros
+              </Link>
             </li>
           </ul>
         </div>
@@ -44,4 +74,4 @@ const Header = () => {
   );
 };
 
-export default Header; 
+export default Header;
